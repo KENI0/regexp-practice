6b09e111e483776f46dcc821c92b785fca5bd7cb
KENI0 <chl0933@naver.com>
2022-08-11 17:12:20 +0900

    Create Project

---
+++ main.js
@@ -0,0 +1,11 @@
+  let str = `
+  010-1234-5678
+  Chl0933@naver.com
+  https://www.naver.com 
+  The play ground chl dog cat jumping all around.
+  fruits watermelon the 
+  `
+
+  console.log(
+    str.match(/./g)
+    )
